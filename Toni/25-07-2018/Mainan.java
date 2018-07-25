5eb0cf6b3ddf93bf27d37f0cd8138faed6582d68
Tony Hazama <tonyhazama4721@gmail.com>
2018-07-25 15:50:36 +0700

    25-07-2018

---
+++ Toni/25-07-2018/Mainan.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Mainan{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int mainan = 0, terjual = 0;
+
+		System.out.print("Berapa total belanja anda ? ");
+
+		}
+
+	
+}
