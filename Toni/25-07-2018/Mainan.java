e6b6fa25be74a3b525e75395a81e682c7ed34f22
Tony Hazama <tonyhazama4721@gmail.com>
2018-07-26 09:35:14 +0700

    25-07-2018 v.2

--- Toni/25-07-2018/Mainan.java
+++ Toni/25-07-2018/Mainan.java
@@ -2,11 +2,28 @@ import java.util.*;
 public class Mainan{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		int mainan = 0, terjual = 0;
+		Random rand = new Random();
+		int mainan = 0, produksi = 0, terjual = 0, saldo = 13000000;
 
-		System.out.print("Berapa total belanja anda ? ");
+	
+		for (int hari = 0; hari < 7; hari++) {
+			System.out.println("\nHari Ke-"+(hari+1)+"\n------------------------------");
+			produksi = rand.nextInt(500)+1501;
+			saldo -= (produksi * 5000);
+			mainan += produksi;
+			terjual = (mainan * (rand.nextInt(50)+51))/100;
+			saldo += terjual * 6500;
+			mainan = mainan - terjual;
+			System.out.println("Mainan Diproduksi : "+produksi);
+			System.out.println("Mainan Dijual     : "+terjual);
+			System.out.println("Sisa Mainan       : "+mainan);
+			System.out.println("Saldo dipakai     : "+(produksi * 5000));
+			System.out.println("Saldo didapat     : "+(terjual * 6500));
+			System.out.println("------------------------------\nSisa Saldo        : "+saldo+"\n");
 
 		}
+
+	}
 
 	
 }
